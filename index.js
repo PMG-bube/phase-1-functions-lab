fec6cf6925a35b846d2f8c09301b5200e9530033
pmg <gachigipm@gmail.com>
2024-07-14 23:10:08 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(somevalue){
+    if(somevalue<42){
+        return (42-somevalue)
+    }else{
+        return(somevalue-42)
+    }
+}
+    function distanceFromHqInFeet(somevalue){
+        return distanceFromHqInBlocks(somevalue)*264
+
+    }
+    function distanceTravelledInFeet(a,b){
+        if(b>a){
+            return ((b-a)*264)
+        }else{ return ((a-b)*264)
+    }
+}
+function calculatesFarePrice(start,destination){
+    let distance=distanceTravelledInFeet(start,destination);
+    if(distance<=400){
+return 0;
+    }else if(distance>=400&&distance<=2000){
+        return (distance-400)*0.02;
+    }else if(distance>=2000&&distance<=2500){
+        return 25;
+    }else{
+        return 'cannot travel that far'
+    }
+}
