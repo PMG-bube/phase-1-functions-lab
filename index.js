23df46e7811cd1d3caa62a8ebb3898b865cfd715
pmg <gachigipm@gmail.com>
2024-09-28 08:01:23 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street){
+   if(street>42){ return (street-42)
+}  else{ return 42-street}};
+function distanceFromHqInFeet(street){
+    return (distanceFromHqInBlocks(street)*264)
+}
+function distanceTravelledInFeet(start,end){
+    if (end>start){
+        return (end-start)*264
+}else {return(start-end)*264}}
+function calculatesFarePrice(start,end){
+let distance=distanceTravelledInFeet(start,end);
+if (distance<=400){
+    return 0;
+}else if (distance>400&&distance<=2000){
+    return (distance-400)*0.02;
+}else if(distance>2000&&distance<=2500){
+    return 25;
+}
+else{
+    return "cannot travel that far";
+}
+}
